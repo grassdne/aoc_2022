3430f2cd900167937bcb06d1374247d5218810d9
grassdne <ziarustum@gmail.com>
2022-12-09 14:20:22 -0500

    improve

--- 08/src/main.rs
+++ 08/src/main.rs
@@ -8,8 +8,7 @@ fn main() {
         .expect("able to open the input file");
 
     let items: Vec<Vec<u32>> = items.lines()
-        .map(|ln| ln.as_bytes()
-            .iter()
+        .map(|ln| ln.as_bytes().iter()
             .map(|&x| (x - b'0') as u32)
             .collect::<Vec<u32>>()
         ).collect();
@@ -19,40 +18,27 @@ fn main() {
 
     {
         let mut count = 0;
+        let mut best_score = 0;
 
         for (y, ln) in items.iter().enumerate() {
             for (x, &it) in ln.iter().enumerate() {
                 let horz_greater = |i: usize| ln[i] >= it;
                 let vert_greater = |i: usize| items[i][x] >= it;
+
                 if (x+1..cols).any(horz_greater) && (0..x).any(horz_greater)
-                && (y+1..rows).any(vert_greater) && (0..y).any(vert_greater) {
+                && (y+1..rows).any(vert_greater) && (0..y).any(vert_greater)) {
                     count += 1;
                 }
+
+                let scenic_score = ((x+1..cols).find(|&i| horz_greater(i)).unwrap_or(cols-1) - x)
+                                 * ((y+1..rows).find(|&i| vert_greater(i)).unwrap_or(rows-1) - y)
+                                 * (x - (0..x).rev().find(|&i| horz_greater(i)).unwrap_or(0))
+                                 * (y - (0..y).rev().find(|&i| horz_greater(i)).unwrap_or(0));
+                best_score = best_score.max(scenic_score);
             }
         }
         
         println!("[PART ONE] {}", cols*rows - count);
+        println!("[PART TWO] {}", best_score);
     }
-
-    let mut best_score = 0;
-    for (y, ln) in items.iter().enumerate() {
-        for (x, &it) in ln.iter().enumerate() {
-            let scenic_score = count(&mut ln.into_iter().take(x).rev().cloned(), it)
-                             * count(&mut ln.into_iter().skip(x+1).cloned(), it)
-                             * count(&mut items.iter().map(|ln| ln[x]).take(y).rev(), it)
-                             * count(&mut items.iter().map(|ln| ln[x]).skip(y+1), it);
-            best_score = best_score.max(scenic_score);
-        }
-    }
-    println!("[PART TWO] {}", best_score);
-}
-
-// I can't just take_while because I want top stop *after* the failing element not before
-fn count(items: &mut dyn std::iter::Iterator<Item = u32>, it: u32) -> u32 {
-    let mut score = 0;
-    for i in items {
-        score += 1;
-        if i >= it { break; }
-    }
-    score
 }
