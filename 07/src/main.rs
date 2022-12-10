608d07758486eeefdade153533eab64b618c1485
grassdne <ziarustum@gmail.com>
2022-12-10 14:17:37 -0500

    07 use lines iterator instead of indices

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -1,27 +1,30 @@
 use std::env;
 use std::fs;
+use std::str::Lines;
+use std::iter::Peekable;
 
-fn chdir(lines: &Vec<&str>, i: &mut usize, sizes: &mut Vec<u32>) -> u32 {
+fn push_dir(lines: &mut Peekable<Lines>, sizes: &mut Vec<u32>) -> u32 {
     let mut size = 0;
-    while *i < lines.len() {
-        let cmd = lines[*i];
+    while lines.peek().is_some() {
+        let cmd = lines.next().unwrap();
         match &cmd[2..4] {
             "cd" => {
-                *i += 1;
                 if &cmd[5..] == ".." {
                     break;
                 } else {
-                    size += chdir(lines, i, sizes);
+                    size += push_dir(lines, sizes);
                 }
             }
             "ls" => {
-                while {*i += 1; *i < lines.len() } {
-                    match lines[*i].split_once(' ').unwrap() {
-                        ("dir", _name) => continue,
-                        ("$", _cd) => break,
-                        (sz, _name) => {
-                            size += sz.parse::<u32>().unwrap()
-                        },
+                loop {
+                    match lines.peek().map(|ln| ln.split_once(' ').unwrap()) {
+                        Some(("dir", _name)) => { lines.next(); }
+                        Some(("$", _cd)) => break,
+                        Some((sz, _name)) => {
+                            size += sz.parse::<u32>().unwrap();
+                            lines.next();
+                        }
+                        None => break,
                     };
                 }
             }
@@ -37,12 +40,10 @@ fn main() {
         .expect("an input file argument");
     let lines = fs::read_to_string(name)
         .expect("able to open the input file");
-    let lines: Vec<&str> = lines.lines().collect();
-    let mut i: usize = 0;
-    assert_eq!(lines[i], "$ cd /");
-    i += 1;
+    let mut lines = lines.lines().peekable();
+    assert_eq!(lines.next(), Some("$ cd /"));
     let mut sizes = Vec::new();
-    let used_space = chdir(&lines, &mut i, &mut sizes);
+    let used_space = push_dir(&mut lines, &mut sizes);
     println!("[PART ONE]: {}", sizes.iter().filter(|&&x| x <= 100000).sum::<u32>());
 
     const TOTAL_DISK_SPACE: u32 = 70000000;
